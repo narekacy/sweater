4b485645d65ae46c004353ffe2614191f4d34508
narekacy <gurjiev42@gmail.com>
2020-05-20 00:23:23 +0300

    Add tag search filter

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -37,5 +38,15 @@ public class GreetingController {
         return "main";
     }
 
-
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter == null || !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+        model.put("messages", messages);
+        return "main";
+    }
 }
--- src/main/java/com/example/sweater/MessageRepository.java
+++ src/main/java/com/example/sweater/MessageRepository.java
@@ -2,5 +2,9 @@ package com.example.sweater;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
 }
